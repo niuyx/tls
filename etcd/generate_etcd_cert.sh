3deac50b048ed84aa30b15b763dcf7e4e28751be
niuyx <niuyunxiao@foxmail.com>
2020-10-09 00:24:59 +0800

    verify etcd

--- etcd/generate_etcd_cert.sh
+++ etcd/generate_etcd_cert.sh
@@ -63,5 +63,5 @@ EOF
 cfssl gencert -initca ca-csr.json | cfssljson -bare ca -
 cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=www server-csr.json | cfssljson -bare server
 
-ls etcd/*.pem
+ls *.pem
 
