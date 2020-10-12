5e074ebdc086d7c31266bcd6161264e6ca94589c
niuyx <niuyx@niuyx.cc>
2020-10-12 19:00:23 +0800

    verify

--- cfssl.sh
+++ cfssl.sh
@@ -4,5 +4,5 @@
 chmod +x cfssl_linux-amd64 cfssljson_linux-amd64 cfssl-certinfo_linux-amd64
 mv cfssl_linux-amd64 /usr/local/bin/cfssl
 mv cfssljson_linux-amd64 /usr/local/bin/cfssljson
-mv cfssl-certinfo_linux-amd64 /usr/bin/cfssl-certinfo
+mv cfssl-certinfo_linux-amd64 /usr/local/bin/cfssl-certinfo
 echo "`cfssl version`"
